51d702598d2b73160897167f2c690c32de7c1548
mahdistz <mahdis.taghizadeh1376@gmail.com>
2023-10-19 23:09:02 +0330

    fix url address of blog

--- Blog/config/urls.py
+++ Blog/config/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog_post.urls')),
+    path('', include('blog_post.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
