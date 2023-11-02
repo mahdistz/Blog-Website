f595b4f51d359df5e9c5c66742b3cdf16eef1b8a
mahdistz <mahdis.taghizadeh1376@gmail.com>
2023-11-02 22:15:16 +0330

    fix media files showing when debug is false

--- config/urls.py
+++ config/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_post.urls')),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'blog_post.views.custom_404_view'
